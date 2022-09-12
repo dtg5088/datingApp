61f38dc1c4c08a869328b068219d8a94daab6b31
DTGillaird <dtg5088@gmail.com>
2022-09-12 13:31:03 -0400

    feat: creating initial project

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "deviousDev28");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "frankobeans");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "cupcakeWarrior");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "slothGurl22");
